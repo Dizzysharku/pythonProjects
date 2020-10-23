3f06ad47b49fb02f95d5010877b4bc442e66d346
Yen <62843535+Dizzysharku@users.noreply.github.com>
2020-10-23 14:38:17 +0100

    Basic Main.py Function.py and System.py
    Start of project, most of the files where finished offline.

---
+++ employeeClockIn/Library/System.py
@@ -0,0 +1,13 @@
+class Messages():
+    def ERROR():
+        print("""--------------------
+- Error with Input -
+--------------------""")
+    def registerError():
+        print("""----------------------------
+-You are already registered-
+----------------------------""")
+class Separator():
+    def Line():
+        print("----------------------------")
+        
---
+++ employeeClockIn/Main.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.insert(1, '../employeeClockIn/Library')
+from Library import Function
+from Library import System
+from datetime import datetime
+def Menu():
+    Identity = Function.inForm.Identification()
+    System.Separator.Line()
+    print("\tWelcome {} {}".format(Identity['First Name'],Identity['First Letter of Last Name']))
+    while True:
+        try:
+            System.Separator.Line()
+            choice = int(input("""\tMenu:
+            1. Registered Employees Today
+            2. Register yourself
+            3. Logout
+            4. Quit
+            What option do you pick: """))
+            System.Separator.Line()
+            if choice == 1:
+                Function.inFile.employeeRegister()
+            elif choice == 2:
+                Function.inFile.Register(Identity)
+            elif choice == 3:
+                Identity = Function.inForm.Identification()
+            elif choice == 4:
+                break
+            else:
+                System.Messages.ERROR()
+                pass
+        except:
+            pass
+
+Menu()
