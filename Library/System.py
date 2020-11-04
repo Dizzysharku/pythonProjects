b0f5da09f45565203730f2149b1dbfebcdccf080
Yen <62843535+User25514@users.noreply.github.com>
2020-11-04 21:16:10 +0000

    Removed a sub folder

---
+++ Library/System.py
@@ -0,0 +1,20 @@
+#
+#   Message Error Pulling
+#
+class Messages():
+    def ERROR():
+        print("""--------------------
+- Error with Input -
+--------------------""")
+    def registerError():
+        print("""----------------------------
+-You are already registered-
+----------------------------""")
+    def invalidFile():
+        print("""--------------
+-Invalid File-
+--------------""")
+class Separator():
+    def Line():
+        print("----------------------------")
+        
